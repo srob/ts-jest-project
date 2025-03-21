348edef6794764e77d3bf76031248950989dc4dd
srob <sjroberts@gmail.com>
2025-03-21 12:13:12 +0000

    initial commit

---
+++ tests/fizzbuzz.test.ts
@@ -0,0 +1,14 @@
+import { fizzBuzz } from '../src/fizzbuzz';
+
+test('returns Fizz for 3', () => {
+  expect(fizzBuzz(3)).toBe("Fizz");
+});
+
+test('returns Buzz for 5', () => {
+  expect(fizzBuzz(5)).toBe("Buzz");
+});
+
+test('returns FizzBuzz for 15', () => {
+  expect(fizzBuzz(15)).toBe("FizzBuzz");
+});
+
